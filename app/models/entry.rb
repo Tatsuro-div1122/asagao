f2f1ea42d65bce77780cd795d00bd77d940f9b9d
Tatsuro <tatsurow1122@gmail.com>
2020-09-12 23:16:32 +0900

    [Add]entryモデル　バリデーション　スコープ

---
+++ app/models/entry.rb
@@ -0,0 +1,16 @@
+class Entry < ApplicationRecord
+  belongs_to :author, class_name: "Member", foreign_key: "member_id"
+
+  STATUS_VALUES = %w(draft member_only public)
+
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :body, :posted_at, presence: true
+  validates :status, inclusion: { in: STATUS_VALUES }
+
+  scope :common, -> { where(status: "public") }
+  scope :published, -> { where("status <> ?", "draft") }
+  scope :full, ->(member) {
+    where("status <> ? OR member_id = ?", "draft", member.id)
+  }
+  scope :readable_for, ->(member) { member? full(member): common }
+end
